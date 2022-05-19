f6fe5b7d703c73bdc1748a056ae0b015488d8c63
Hamukaze <maplecow@gmail.com>
2022-05-19 20:04:44 +0900

    Added click event for numButtons

--- script.js
+++ script.js
@@ -30,3 +30,20 @@ function operate (a, operator, b){
             break;
     }
 }
+
+function updateDisplay (num) {
+    if (displayNum[0].innerHTML == "0") {
+        displayNum[0].innerHTML = num;
+        return;
+    }
+    displayNum[0].innerHTML += num;
+    return;
+}
+
+let displayNum = document.getElementsByClassName("display-num");
+let buttons = document.querySelectorAll(".numButton");
+console.log(displayNum);
+buttons.forEach(button => button.addEventListener("click", function(){
+    updateDisplay(button.id);
+}));
+console.log(displayNum);
